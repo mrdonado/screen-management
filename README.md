f1baf91c826564807fefcdaf04fe51186a5a50f4
F. Javier R. Donado <fjrd84@gmail.com>
2017-08-28 18:44:46 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,40 @@
+# screen-management
+
+[An npm module](https://www.npmjs.com/package/screen-management) to turn the computer screen off and on again.
+
+It only works on windows systems for now, but a Linux version is coming soon.
+
+## Install the module
+
+Import it into your project with the following command:
+
+`npm install screen-management`
+
+Or if you prefer yarn to npm install:
+
+`yarn add screen-management` 
+
+## How to use it
+
+You can use this module within your code like this.
+
+```js
+import screen from 'screen-management';
+
+// Call this function to turn the computer screen to sleep
+screen.turnOff(); 
+
+// Call this function to turn on the screen (in case it was in sleep mode)
+screen.turnOn(); 
+```
+
+## Dependencies
+
+No dependencies are required. The module runs native windows executables compiled using the following C# projects:
+
+- https://github.com/fjrd84/windows-sleep-screen
+- https://github.com/fjrd84/windows-wake-up-screen
+
+## Credits
+
+Developed by [F. Javier R. Donado](https://www.jdonado.com) for [netvico GmbH](http://www.netvico.com)
---
+++ src/main.js
@@ -0,0 +1,58 @@
+const {
+  exec
+} = require('child_process'),
+  os = require('os');
+
+
+//---------------------WIN32-------------------------------//
+const win32 = {};
+
+win32.turnOff = () => {
+  exec(__dirname + '/../bin/sleep-screen.exe');
+};
+
+win32.turnOn = () => {
+  exec(__dirname + '/../bin/wake-up-screen.exe');
+};
+
+//---------------------/WIN32-------------------------------//
+
+
+// Functions of all the platforms put together
+const platforms = {
+  win32
+};
+
+
+//---------------------UTILITIES----------------------------//
+/**
+ * Function to check if the current operating system is supported by
+ * this library or not.
+ * @returns {boolean}
+ */
+const supported = () => {
+  return Object.keys(platforms).indexOf(os.platform()) > -1;
+};
+
+/**
+ * This function throws an error when the current operating system is not
+ * supported.
+ */
+const platformCheck = () => {
+  if (!supported()) {
+    throw Error('Error (screen-management): Your platform is not supported yet.');
+  }
+};
+//---------------------/UTILITIES---------------------------//
+
+
+module.exports = {
+  turnOff: () => {
+    platformCheck();
+    platforms[os.platform()].turnOff();
+  },
+  turnOn: () => {
+    platformCheck();
+    platforms[os.platform()].turnOff();
+  }
+};
